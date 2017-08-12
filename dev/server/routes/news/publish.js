22a332b3d8622af7e38d6ab93ab3c872ff92209d
Brandon Da Silva <brandon@bdsdesign.co>
2017-08-12 17:41:55 -0400

    Testing publishing

--- dev/server/routes/news/publish.js
+++ dev/server/routes/news/publish.js
@@ -21,18 +21,9 @@ router.post ('/', function(req, res) {
 
   console.log(req.body);
 
-  var actions = JSON.parse(req.body['payload']);
+  if (req.body['payload'] == undefined) {
 
-  var originalMessage = actions['original_message'];
-  originalMessage['attachments'][2]['actions'] = [];
-  var payload = originalMessage['attachments'][2];
-
-  if (actions["actions"][0]["value"] == "no") {
-    payload["text"] = "Not Published. You can check out the added posting on Webflow Editor."
-
-  } else if (actions["actions"][0]["value"] == "publish") {
-    payload["text"] = "Published!"
-
+    // For publishing using slash commands on Slack
     // Publish on Webflow
     var publish = webflow.publishSite({
       siteId: '58be0ec3d9b8619b4a64d0d3',
@@ -41,23 +32,67 @@ router.post ('/', function(req, res) {
 
     publish.then(p => console.log(p));
 
-  } else {
-    payload["text"] = "Sorry, that didn't work. Please check Webflow for errors."
-  }
-
-  originalMessage['attachments'][2] = payload;
-
-  request({
-    url: actions['response_url'],
-    method: "POST",
-    json: true,
-    body: originalMessage,
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
+    request({
+      url: req.body['response_url'],
+      method: "POST",
+      json: true,
+      body: {
+        "attachments": [
+          {
+            "title": "Published to Webflow",
+            "color": "#36a64f",
+            "text": "The latest version of the website has been posted!",
+          }
+        ]
+      },
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+        }
       }
+    });
+
+  } else {
+
+    // Checking button response after a preview was sent to Slack
+    var actions = JSON.parse(req.body['payload']);
+
+    var originalMessage = actions['original_message'];
+    originalMessage['attachments'][2]['actions'] = [];
+    var payload = originalMessage['attachments'][2];
+
+    if (actions["actions"][0]["value"] == "no") {
+      payload["text"] = "Not Published. You can check out the added posting on Webflow Editor."
+
+    } else if (actions["actions"][0]["value"] == "publish") {
+      payload["text"] = "Published!"
+
+      // Publish on Webflow
+      var publish = webflow.publishSite({
+        siteId: '58be0ec3d9b8619b4a64d0d3',
+        domains: ['mtg2017.webflow.io']
+      });
+
+      publish.then(p => console.log(p));
+
+    } else {
+      payload["text"] = "Sorry, that didn't work. Please check Webflow for errors."
     }
-  });
+
+    originalMessage['attachments'][2] = payload;
+
+    request({
+      url: actions['response_url'],
+      method: "POST",
+      json: true,
+      body: originalMessage,
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+        }
+      }
+    });
+  }
 });
 
 module.exports = router;
