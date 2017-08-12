242ff3c594fd71a644f4a2d0ece94934f3bf69a5
Brandon Da Silva <brandon@bdsdesign.co>
2017-08-11 21:11:26 -0400

    Added support for SEO crawling

--- dev/server/routes/news.js
+++ dev/server/routes/news.js
@@ -21,5 +21,67 @@ router.get ('/', function(req, res) {
 router.post ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
 
-  // var client = new MetaInspector("http://www.google.com", { timeout: 5000 });
+  console.log(req.body);
+
+  var client = new MetaInspector(req.body['url'], { timeout: 5000 });
+
+  console.log(client);
+
+  // Create Webflow item to push to the CMS
+  var item = webflow.createItem({
+    collectionId: '',
+    fields: {
+      'name': ,
+      'slug': ,
+      '_archived': ,
+      '_draft': ,
+      'article-link': ,
+      'image-link':
+    }
+  });
+
+  // HTTP POST to Slack Webhook to post an update on Slack
+  /*request({
+    url: process.env.BDS_SLACK_WEBHOOK,
+    method: "POST",
+    json: true,
+    body: {
+      "attachments": [
+        {
+          "fallback": "A new post from Instagram has been posted to Webflow.",
+          "color": "#36a64f",
+          "pretext": "A new post from Instagram has been posted to Webflow.",
+          "title": "Instagram Post to Webflow",
+          "text": "This needs to be published to the Webflow CMS using the Webflow Editor",
+          "fields": [
+            {
+              "title": "Name",
+              "value": req.body['name'],
+              "short": false
+            }, {
+              "title": "Post Link",
+              "value": req.body['link'],
+              "short": false
+            }, {
+              "title": "Image Link",
+              "value": req.body['image'],
+              "short": false
+            }
+          ]
+        }
+      ]
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    }
+  });
+
+  item.then(i => console.log(i)); // Send to Webflow
+
+  */
+
+  res.send(req.body);
 });
+
+module.exports = router;
