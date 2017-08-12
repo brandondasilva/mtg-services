3950562f22b068e0b0c4e1d1387b34cbafc6a1c6
Brandon Da Silva <brandon@bdsdesign.co>
2017-08-12 01:20:08 -0400

    Edits

--- dev/server/routes/publish.js
+++ dev/server/routes/publish.js
@@ -33,6 +33,7 @@ router.post ('/', function(req, res) {
 
   console.log(actions);
   console.log(actions['actions']);
+  console.log(actions[0]);
 
   if (actions["actions"][0]["value"] == "no") {
     payload = { "text": "Not Published. You can check out the added posting on Webflow Editor." };
