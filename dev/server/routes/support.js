f44f9effee76c11d1899b1458a43959c972882c4
Brandon Da Silva <brandon@bdsdesign.co>
2017-08-15 18:20:34 -0400

    Added support request functionality

--- dev/server/routes/support.js
+++ dev/server/routes/support.js
@@ -1,10 +1,11 @@
 
 'use strict';
 
-var moment = require('moment-timezone');
-
 var express = require('express');
 var router = express.Router();
+
+var helper = require('sendgrid').mail;
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 router.get ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
@@ -14,4 +15,55 @@ router.get ('/', function(req, res) {
 router.post ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
 
+  req.body['name'] = req.body['firstname'] + ' ' + req.body['lastname'];
+
+  var from_email = new helper.Email('support@medtechgateway.com', "Medical Technologies Gateway Support");
+  var user_email = new helper.Email(req.body['email'], req.body['name']);
+  var subject = "Support submission to MTG";
+
+  var request = composeMail(from_email, subject, user_email, req.body, process.env.SUPPORT_TEMPLATE);
+
+  sg.API(request, function(error, response) {
+    // Log response
+    console.log('--RESPONSE BEGIN--');
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+    console.log('--RESPONSE END--\n');
+  });
+
 });
+
+/**
+ * Set up the mail information and template to be requested to be sent through SendGrid
+ *
+ * @param {String} from_email "From" email
+ * @param {String} subject Subject for the email
+ * @param {String} to_email "To" email
+ * @param {Object} form_data The information submitted on the form
+ * @param {String} template_id The ID of the template to use when sending the email
+ */
+function composeMail(from_email, subject, to_email, form_data, template_id) {
+
+  var content = new helper.Content("text/html", form_data['issue-description']);
+
+  var mail = new helper.Mail(from_email, subject, to_email, content); // Create mail helper
+
+  // Set up personalizations for the email template using the form data from the parameters
+  mail.personalizations[0].addSubstitution( new helper.Substitution('-name-', form_data['name']) );
+  mail.personalizations[0].addSubstitution( new helper.Substitution('-firstname-', form_data['firstname']) );
+  mail.personalizations[0].addSubstitution( new helper.Substitution('-email-', form_data['email']) );
+  mail.personalizations[0].addSubstitution( new helper.Substitution('-issue-', form_data['issue-title']) );
+  // mail.personalizations[0].addSubstitution( new helper.Substitution('-description-', form_data['issue-description']) );
+
+  mail.setTemplateId(template_id); // Set the Template ID for the email content
+
+  // Return request to send to the SendGrid API
+  return sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+}
+
+module.exports = router;
