2e495fc59b9b2a777972cf799bfa146ca94f81b0
Brandon Da Silva <brandon@bdsdesign.co>
2017-08-15 16:45:47 -0400

    Added URL checking for inputs

--- dev/server/routes/news/news.js
+++ dev/server/routes/news/news.js
@@ -13,6 +13,8 @@ var webflow = new Webflow({ token: process.env.WEBFLOW_TOKEN });
 
 var MetaInspector = require('node-metainspector');
 
+var validUrl = require('valid-url');
+
 router.get ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send('API v1 GET: Hello World!');
@@ -22,121 +24,143 @@ router.post ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
 
   console.log(req.body);
-  res.send("Sending link...");
 
+  // Sets the 'url' field of the newsPost object depending on where the URL was passed
   if (req.body['url'] == undefined) {
     var newsPost = { 'url': req.body['text'] }
   } else {
     var newsPost = { 'url': req.body['url'] }
   }
 
-  var client = new MetaInspector(newsPost['url'], { timeout: 5000 });
+  // Checks to see if the valid passed is a valid URL
+  if (validUrl.isUri(newsPost['url'])) {
 
-  client.on("fetch", function() {
+    // If the input string is a valid URL
 
-    if (req.body['title'] == undefined) {
-      newsPost['title'] = client.title;
-    } else {
-      newsPost['title'] = req.body['title'];
-    }
+    res.send("Sending link...");
 
-    if (req.body['description'] == undefined) {
-      newsPost['description'] = client.description;
-    } else {
-      newsPost['description'] = req.body['description'];
-    }
+    var client = new MetaInspector(newsPost['url'], { timeout: 5000 });
 
-    if (req.body['image'] == undefined) {
-      newsPost['image'] = client.image;
-    } else {
-      newsPost['image'] = req.body['image'];
-    }
+    client.on("fetch", function() {
 
-    // Create Webflow item to push to the CMS TODO ADD TO FEATURED AUTOMATICALLY
-    var item = webflow.createItem({
-      collectionId: '58be4ff264167da73c14db28',
-      fields: {
-        'name': newsPost['title'],
-        'slug': 'news-post',
-        '_archived': false,
-        '_draft': false,
-        'article-link': newsPost['url'],
-        'image-link': newsPost['image'],
-        'meta-description': newsPost['description']
+      if (req.body['title'] == undefined) {
+        newsPost['title'] = client.title;
+      } else {
+        newsPost['title'] = req.body['title'];
       }
+
+      if (req.body['description'] == undefined) {
+        newsPost['description'] = client.description;
+      } else {
+        newsPost['description'] = req.body['description'];
+      }
+
+      if (req.body['image'] == undefined) {
+        newsPost['image'] = client.image;
+      } else {
+        newsPost['image'] = req.body['image'];
+      }
+
+      // Create Webflow item to push to the CMS TODO ADD TO FEATURED AUTOMATICALLY
+      var item = webflow.createItem({
+        collectionId: '58be4ff264167da73c14db28',
+        fields: {
+          'name': newsPost['title'],
+          'slug': 'news-post',
+          '_archived': false,
+          '_draft': false,
+          'article-link': newsPost['url'],
+          'image-link': newsPost['image'],
+          'meta-description': newsPost['description'],
+          'featured': true
+        }
+      });
+
+      item.then(i => console.log(i)); // Send to Webflow
+
+      var content = {
+        "attachments": [
+          {
+            "title": "New News Post to Webflow",
+            "color": "#36a64f",
+            "text": "This needs to be published to the Webflow CMS using the Webflow Editor",
+            "image_url": newsPost['image']
+          },
+          {
+            "title": "Article Information",
+            "fields": [
+              {
+                "title": "Name",
+                "value": newsPost['title'],
+                "short": false
+              }, {
+                "title": "Description",
+                "value": newsPost['description'],
+                "short": false
+              }, {
+                "title": "Article Link",
+                "value": newsPost['url'],
+                "short": false
+              }
+            ],
+          },
+          {
+            "fallback": "Publish to Webflow?",
+            "title": "Publish to Webflow?",
+            "callback_id": "publish_webflow",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+              {
+                "name": "publish",
+                "text": "Publish",
+                "style": "danger",
+                "type": "button",
+                "value": "publish"
+              },
+              {
+                "name": "no",
+                "text": "No Thanks",
+                "type": "button",
+                "value": "no"
+              }
+            ]
+          }
+        ]
+      }
+
+      slackRequest(content, process.env.BDS_SLACK_WEBHOOK);
+      slackRequest(content, process.env.PREMUS_SLACK_WEBHOOK);
     });
 
-    item.then(i => console.log(i)); // Send to Webflow
+    client.on("error", function(err){
+      console.log(err);
+    });
 
-    var content = {
-      "attachments": [
-        {
-          "title": "New News Post to Webflow",
-          "color": "#36a64f",
-          "text": "This needs to be published to the Webflow CMS using the Webflow Editor",
-          "image_url": newsPost['image']
-        },
-        {
-          "title": "Article Information",
-          "fields": [
-            {
-              "title": "Name",
-              "value": newsPost['title'],
-              "short": false
-            }, {
-              "title": "Description",
-              "value": newsPost['description'],
-              "short": false
-            }, {
-              "title": "Article Link",
-              "value": newsPost['url'],
-              "short": false
-            }
-          ],
-        },
-        {
-          "fallback": "Publish to Webflow?",
-          "title": "Publish to Webflow?",
-          "callback_id": "publish_webflow",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "publish",
-              "text": "Publish",
-              "style": "danger",
-              "type": "button",
-              "value": "publish"
-            },
-            {
-              "name": "no",
-              "text": "No Thanks",
-              "type": "button",
-              "value": "no"
-            }
-          ]
-        }
-      ]
-    }
+    client.fetch();
 
-    slackRequest(content, process.env.BDS_SLACK_WEBHOOK);
-    slackRequest(content, process.env.PREMUS_SLACK_WEBHOOK);
-  });
+  } else {
 
-  client.on("error", function(err){
-    console.log(err);
-  });
+    // If the input string is not a valid URL
 
-  client.fetch();
+    res.status(500).send("Failed! Please enter a valid URL.");
+  }
+
 });
 
-function slackRequest(content, webhook) {
+/**
+ * Post the content being passed into the function to Slack through the webhook
+ *
+ * @param {Object} data The content to populate the Slack post
+ * @param {Object} webhook The content to populate the Slack post
+ */
+function slackRequest(data, webhook) {
 
   request({
     url: webhook,
     method: "POST",
     json: true,
-    body: content,
+    body: data,
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         console.log(body);
